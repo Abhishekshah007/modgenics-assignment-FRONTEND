360e59d070e8aaa0a42b6d992b809788d468b907
Abhishek Shah <amitkumarshah499@gmail.com>
2024-11-19 06:09:17 +0530

    Initial commit #3

--- src/components/captchaBox/CaptchaBox.jsx
+++ src/components/captchaBox/CaptchaBox.jsx
@@ -117,12 +117,12 @@ const handlePayout = async () => {
   return (
     <>
       {!captchaVisible && (
-        <div className="flex justify-center items-center w-64">
+        <div className="flex justify-center items-center w-64" onChange={(e) => setName(e.target.value)}>
           <input
             type="text"
             placeholder="Enter your user name"
             value={name}
-            onChange={(e) => setName(e.target.value)}
+            
             className="w-full border border-gray-300 rounded-md py-2 px-4 text-sm shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-400"
           />
         </div>
